a3da3833fdd247c18cf4877ea17e2bfb1a6655b5
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-09-24 17:50:13 +0100

    Add validation for username input field

--- script.js
+++ script.js
@@ -13,18 +13,41 @@ function showSuccess(input) {
   formControl.className = 'form-group success';
 }
 
-function checkRequired(formInputs) {
+function containsWhiteSpace(string) {
+  return /\s/.test(string);
+}
+
+function checkInputLength(formInput, min, max) {
+  if (formInput.value.length < min) {
+    showError(formInput, `must be at least ${min} characters`);
+  } else if (formInput.value.length > max) {
+    showError(formInput, `must be less than ${max} characters`);
+  } else if (containsWhiteSpace(formInput.value)) {
+    console.log(containsWhiteSpace(formInput.value));
+    showError(formInput, 'must have no white spaces');
+  } else {
+    showSuccess(formInput);
+  }
+}
+
+function checkinputsValidity(formInputs) {
   console.log(formInputs);
   formInputs.forEach((input) => {
-    if (input.value === '') {
+    if (input.value.trim() === '') {
       showError(input, 'is required');
+      console.log('not a name or password');
       return;
+    } else if (input.id === 'username') {
+      checkInputLength(input, 3, 20);
     }
-    showSuccess(input);
   });
+}
+
+function checkUserName(formInputs) {
+  formInputs;
 }
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  checkRequired(inputs);
+  checkinputsValidity(inputs);
 });
